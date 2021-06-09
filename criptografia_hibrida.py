4143051e3b718476d8f0d4b02e0cb3b519232f56
Oswaldo Aguilar Martinez <69224700+Waldo97Ag@users.noreply.github.com>
2021-06-09 15:02:41 -0500

    final

--- criptografia_hibrida.py
+++ criptografia_hibrida.py
@@ -100,10 +100,11 @@ def seleccionar_funcion():
             message_sent = message.get()   #Mensaje que Alicia manda
             message_file = open("message.txt", "w",encoding='ISO-8859-1')
             with open('message.txt') as f:
-                message_file.write(message_sent) #Escribir mensaje a archivo
+                message_as_bytes=bytes(message_sent,'ISO-8859-1')
+                #message_as_bytes = read_file_content_as_bytes('message.txt')
+                iv_ct, key,ct = encrypt_AES_CBC(message_as_bytes)
+                message_file.write(ct) #Escribir mensaje a archivo
                 message_file.close()
-            message_as_bytes = read_file_content_as_bytes('message.txt')
-            iv_ct, key = encrypt_AES_CBC(message_as_bytes)
             cipher_AES_key_with_RSA(key)
 
         elif combo_sel == "Decipher":
@@ -179,13 +180,15 @@ def encrypt_AES_CBC(data):
     ct = b64encode(ct_bytes).decode('ISO-8859-1')
     result = json.dumps({'iv':iv, 'ciphertext':ct})
     print(result)
-    return result, key #Esta madre es la que se cifra con RSA
+    #print(key)
+    return result, key, ct #Esta madre es la que se cifra con RSA
 
 def decrypt_AES_CBC(json_input,key): #Recibe vector iv y texto cifrado
     try:
         b64 = json.loads(json_input)
         iv = b64decode(b64['iv'])
         ct = b64decode(b64['ciphertext'])
+        print(ct.decode('ISO-8859-1'))
         cipher = AES.new(key, AES.MODE_CBC, iv)
         pt1 = unpad(cipher.decrypt(ct), AES.block_size)
         pt=pt1.decode('ISO-8859-1')
@@ -209,10 +212,10 @@ def cipher_AES_key_with_RSA(data):
     ciphertext, tag = cipher_aes.encrypt_and_digest(data)
     file_out.write(bytes("\nFIN", 'ISO-8859-1'))
     [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-    # print(enc_session_key)
-    # print(cipher_aes.nonce)
-    # print(tag)
-    # print(ciphertext)
+    #print(enc_session_key)
+    #print(cipher_aes.nonce)
+    #print(tag)
+    #print(ciphertext)
     file_out.close()
 
 def decipher_AES_key_with_RSA():
@@ -224,7 +227,7 @@ def decipher_AES_key_with_RSA():
     #file_in=bytes(f,'ISO-8859-1')
     mensaje,llave_AES1,Firma = file_in.split("\nFIN")
     file_in1.close()
-    llave_AES=llave_AES1[::]
+    llave_AES=llave_AES1[:-1:]
     file_out = open("message1.txt", "wb")
     file_out.write(bytes(llave_AES, 'ISO-8859-1'))
     file_out.close()
@@ -236,11 +239,11 @@ def decipher_AES_key_with_RSA():
         [ key_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
 
     # print(enc_session_key.decode('ISO-8859-1'))
-    # print(enc_session_key)
-    # print(nonce)
-    # print(tag)
-    # print(ciphertext)
-    # print(tag)
+    #print(enc_session_key)
+    #print(nonce)
+    #print(tag)
+    #print(ciphertext)
+    #print(tag)
     # print(ciphertext)
 # Decrypt the session key with the private RSA key
     cipher_rsa = PKCS1_OAEP.new(private_key)
@@ -249,7 +252,7 @@ def decipher_AES_key_with_RSA():
     # Decrypt the data with the AES session key
     cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
     data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-    #print(data.decode("ISO-8859-1"))
+    #print(data)
     return data
 def generate_digest(message):
     h = SHA1.new()
@@ -262,7 +265,7 @@ def generate_signature(message_to_sign):
     key = RSA.import_key(open('alicia/private_alice.pem').read())
     message_to_sign = message_to_sign.encode("ISO-8859-1")
     h = SHA256.new(message_to_sign)
-    print(h.hexdigest())
+    #print(h.hexdigest())
     signature = pkcs1_15.new(key).sign(h)
     message_signed = signature.decode("ISO-8859-1")
 
@@ -270,7 +273,7 @@ def generate_signature(message_to_sign):
     signed_file.write("\nFIN")
     signed_file.write(message_signed)
     signed_file.close()
-    print(signature)
+    #print(signature)
 
     return signature
 
